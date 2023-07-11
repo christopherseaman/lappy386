ccdb65352bcb7e39ce385528cd7d709ffb87a205
Christopher Seaman <86775+christopherseaman@users.noreply.github.com>
2023-07-11 15:30:23 -0700

    update based on actual rebuild

--- setup.sh
+++ setup.sh
@@ -1,14 +1,14 @@
 ## COPY DOT-FILES
-cat dot-zshrc | tee -a ~/.zshrc
+cat dot-zshrc > ~/.zshrc
 mkdir -p ~/.ssh
-cat dotssh-config | tee -a ~/.ssh/config
+cat dotssh-config > ~/.ssh/config
 
 git config --global user.name 'Christopher Seaman'
 git config --global user.email '86775+christopherseaman@users.noreply.github.com'
 
 ## INSTALL HOMEBREW
 ## https://brew.sh
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 ## INSTALL BREW PACKAGES
 brew tap homebrew/cask-fonts
@@ -18,4 +18,5 @@ brew install --cask $(grep -v '#' casks.lst)
 
 ## INSTALL NVM AND NODE
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
-source .zshrc & nvm install --lts
+source .zshrc
+nvm install --lts
