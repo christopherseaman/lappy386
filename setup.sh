048c5a851b3b5d14430dcf29a952b77712553909
christopherseaman <86775+christopherseaman@users.noreply.github.com>
2024-12-13 19:09:54 -0800

    moved brews

--- setup.sh
+++ setup.sh
@@ -13,8 +13,8 @@ git config --global user.email '86775+christopherseaman@users.noreply.github.com
 ## INSTALL BREW PACKAGES
 brew tap homebrew/cask-fonts
 brew tap teamookla/speedtest
-brew install $(grep -v '#' brews.lst)
-brew install --cask $(grep -v '#' casks.lst)
+brew install $(grep -v '#' brew.lst)
+brew install --cask $(grep -v '#' cask.lst)
 
 ## INSTALL NVM AND NODE
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
