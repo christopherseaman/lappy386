d386eb7221fda3036c3873f267eb68a4162d6ba0
Christopher Seaman <86775+christopherseaman@users.noreply.github.com>
2025-09-18 02:01:27 +0000

    nvim update

--- tools/artifacts/dot-config-nvim/lua/plugins/markdown.lua
+++ tools/artifacts/dot-config-nvim/lua/plugins/markdown.lua
@@ -5,7 +5,7 @@ return {
     ft = "markdown",
     config = function()
       vim.g.vim_markdown_folding_disabled = 1
-      vim.g.vim_markdown_conceal = 2
+      vim.g.vim_markdown_conceal = 0
       vim.g.vim_markdown_conceal_code_blocks = 0
       vim.g.vim_markdown_math = 1
       vim.g.vim_markdown_frontmatter = 1
