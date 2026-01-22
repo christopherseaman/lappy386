210a1bb6a1a1dd22c2e30169a04a0848bc30dd98
christopherseaman <86775+christopherseaman@users.noreply.github.com>
2026-01-22 12:16:25 -0800

    Simplify container startup process
    Removed waiting loop for cloud-init completion.

--- hosts/strongbad/init.sh
+++ hosts/strongbad/init.sh
@@ -21,19 +21,10 @@ echo "==> Applying cloud-config..."
 lxc config set "$CONTAINER_NAME" user.user-data "$(cat /tmp/cloud-config.yaml)"
 
 echo "==> Starting container..."
-lxc start "$CONTAINER_NAME" < /dev/null
-
-echo "==> Waiting for cloud-init to complete..."
-for i in {1..60}; do
-    if lxc exec "$CONTAINER_NAME" -- test -f /home/christopher/.cloud-init-done 2>/dev/null; then
-        echo "==> Cloud-init finished!"
-        break
-    fi
-    echo "    waiting... ($i/60)"
-    sleep 5
-done
+lxc start "$CONTAINER_NAME" 
 
 echo "==> Verifying setup..."
+lxc list
 lxc exec "$CONTAINER_NAME" -- id christopher
 lxc exec "$CONTAINER_NAME" -- hostname
 
